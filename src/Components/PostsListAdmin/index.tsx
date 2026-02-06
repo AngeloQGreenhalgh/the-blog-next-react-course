25c06976704964a014fd7eeefaf3c003a922cd15
Ângelo Quintarelli Greenhalgh <angelogreenhalgh@hotmail.com>
2026-02-06 09:01:38 -0300

    :hammer: Personalização da área administrativa - posts

--- src/Components/PostsListAdmin/index.tsx
+++ src/Components/PostsListAdmin/index.tsx
@@ -1,4 +1,8 @@
 import { findAllPostsAdmin } from '@/lib/post/queries/admin';
+import clsx from 'clsx';
+import { Trash2Icon } from 'lucide-react';
+
+import Link from 'next/link';
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostsAdmin();
@@ -6,7 +10,34 @@ export default async function PostsListAdmin() {
   return (
     <div className='py-16'>
       {posts.map(post => {
-        return <div key={post.id}>{post.title}</div>;
+        return (
+          <div
+            className={clsx(
+              'py-2 px-2',
+              !post.published && 'bg-slate-300',
+              'flex gap-2 items-center justify-between',
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+            {!post.published && (
+              <span className='text-xs text-slate-600 italic'>
+                (Não publicado)
+              </span>
+            )}
+            <button
+              className={clsx(
+                'text-red-500 cursor-pointer transition',
+                '[&_svg]:w-4 [&_svg]:h-4',
+                'hover:scale-120 text-red-700',
+              )}
+              aria-label={`Apagar post: ${post.title}`}
+              title={`Apagar post: ${post.title}`}
+            >
+              <Trash2Icon />
+            </button>
+          </div>
+        );
       })}
     </div>
   );

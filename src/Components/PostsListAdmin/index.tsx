25d47f2cefce2c01bc6d662f7cc20c1fae3b57d7
Ângelo Quintarelli Greenhalgh <angelogreenhalgh@hotmail.com>
2026-02-09 08:24:54 -0300

    :hammer: Implementação inicial do diálogo de confirmação

--- src/Components/PostsListAdmin/index.tsx
+++ src/Components/PostsListAdmin/index.tsx
@@ -1,4 +1,3 @@
-import { deletePostAction } from '@/actions/post/delete-post-action';
 import { findAllPostsAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -25,13 +24,54 @@ export default async function PostsListAdmin() {
                 (Não publicado)
               </span>
             )}
-            <form action={deletePostAction}>
-              <input type='hidden' name='id' defaultValue={post.id} />
-              <DeletePostButton id={post.id} title={post.title} />
-            </form>
+            <DeletePostButton id={post.id} title={post.title} />
           </div>
         );
       })}
+
+      <div
+        className={clsx(
+          'fixed z-50 inset-0 bg-black/50 backdrop-blur-xs',
+          'flex items-center justify-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-sky-100 p-6 rounded-lg max-w-2xl  mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30 text-center',
+          )}
+        >
+          <h3 className='text-xl font-extrabold'>Título do díalogo</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+            dolore, eum harum obcaecati distinctio inventore doloribus sequi
+            deleniti nostrum delectus esse similique fugit assumenda rerum rem
+            maxime ducimus, aliquam illum?
+          </p>
+          <div className='flex items-center justify-around'>
+            <button
+              className={clsx(
+                'bg-slate-200 hover:bg-slate-300 transition text-slate-950',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                'bg-blue-500 hover:bg-blue-600 transition text-blue-50',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
